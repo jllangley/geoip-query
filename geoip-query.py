6b0f9695f38c3250304545df5963811ea5555ab5
orig3n <orig3n@gmail.com>
2016-03-24 20:42:07 -0500

    Added files via upload
    Initial version. Can look up using ./geoip-query.py -i x.x.x.x
    CSV integration coming soon.

---
+++ geoip-query.py
@@ -0,0 +1,27 @@
+import geoip2.database
+import argparse
+import socket
+import sys
+reader = geoip2.database.Reader("/usr/local/lib/python2.7/dist-packages/_geoip_geolite2/GeoLite2-City.mmdb")
+
+parser = argparse.ArgumentParser(description="Geoip lookup for IP addresses")
+parser.add_argument('-f', metavar='file.csv', type=str, help='file with IP addresses comma seperated.')
+parser.add_argument('-i', metavar='x.x.x.x', type=str, help='Insert IP address.')
+args = parser.parse_args()
+
+def ip_lookup_argv():
+	match = reader.city(args.i)
+	dnslookup = socket.gethostbyaddr(args.i)
+	print (str(dnslookup)) + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
+
+if args.f:
+	print args.f
+elif args.i:
+	print ip_lookup_argv()
+else:
+	print """
+optional arguments:
+  -h, --help   show this help message and exit
+  -f file.csv  file with IP addresses comma seperated.
+  -i x.x.x.x   Insert IP address."""
+
