387c38a5539aea5ac5287484e4f8db1cff157113
orig3n <orig3n@gmail.com>
2016-03-29 23:25:15 -0500

    Added files via upload
    Error handling added for failed reverse dns lookups.

--- geoip-query.py
+++ geoip-query.py
@@ -2,6 +2,7 @@ import geoip2.database
 import argparse
 import socket
 import sys
+import re
 reader = geoip2.database.Reader("/usr/local/lib/python2.7/dist-packages/_geoip_geolite2/GeoLite2-City.mmdb")
 
 parser = argparse.ArgumentParser(description="Geoip lookup for IP addresses")
@@ -11,11 +12,25 @@ args = parser.parse_args()
 
 def ip_lookup_argv():
 	match = reader.city(args.i)
-	dnslookup = socket.gethostbyaddr(args.i)
-	print (str(dnslookup)) + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
+	try:
+		dnslookup = socket.gethostbyaddr(args.i)
+		print (str(dnslookup)) + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
+	except socket.herror:
+		print "unkown host" + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
+ 
+def ip_lookup_csv():
+	with open(args.f) as txt_file:
+		ip_read = txt_file.read()
+		for ipaddr in re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_read):
+			match = reader.city(ipaddr)
+			try:
+				dnslookup = socket.gethostbyaddr(ipaddr)
+				print (str(dnslookup)) + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
+			except socket.herror:
+				print "unkown host" + ", " +  (str(match.city.name)) + ", " +  (str(match.country.name)) + ", " +  (str(match.traits.user_type)) + ", " + (str(match.traits.domain))
 
 if args.f:
-	print args.f
+	print ip_lookup_csv()
 elif args.i:
 	print ip_lookup_argv()
 else:
